ef72b72299e5051f6bb0d24f8575d4ee719ae273
Marty Mazurik <marty@martymazurik.com>
2019-02-06 10:16:33 -0800

    push document changes

---
+++ CCA.Services.RepositoryNook/Exceptions/RepoSvcDocumentNotFoundException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CCA.Services.RepositoryNook.Exceptions
+{
+    public class RepoSvcDocumentNotFoundException : ApplicationException
+    {
+        public RepoSvcDocumentNotFoundException() {  }              //ctor1
+        public RepoSvcDocumentNotFoundException(string message) :   //ctor2
+        base(message)
+        { }
+    }
+}
